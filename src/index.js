d7b4adf95b900cbba1d83c70d8bcc288cc4c156d
keshav-c <keshav1985@gmail.com>
2020-11-04 21:18:48 +0530

    Add starter code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import PropTypes from 'prop-types';
+import './style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Calculator(props) {
+  const { name } = props;
+  return (
+    <h1>
+      This is
+      {' '}
+      { name }
+      {' '}
+      Calculator!
+    </h1>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+Calculator.defaultProps = {
+  name: 'React',
+};
+
+Calculator.propTypes = {
+  name: PropTypes.string,
+};
+
+ReactDOM.render(<Calculator name="Keshav's" />, document.getElementById('root'));
