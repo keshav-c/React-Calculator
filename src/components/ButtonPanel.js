b76653bafbfc316a1d7679b80a766e1852c24ed5
keshav-c <keshav1985@gmail.com>
2020-11-12 14:37:30 +0530

    Replace symbols with HTML entities

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,23 +5,28 @@ const ButtonPanel = () => {
   const buttonGroups = [
     {
       groupId: 1,
-      buttons: ['AC', '+/-', '%', '/'],
+      buttons: [
+        String.fromCharCode(65, 67),
+        String.fromCharCode(0x00b1),
+        String.fromCharCode(0x0025),
+        String.fromCharCode(0x00f7),
+      ],
     },
     {
       groupId: 2,
-      buttons: ['7', '8', '9', 'X'],
+      buttons: ['7', '8', '9', String.fromCharCode(0x00d7)],
     },
     {
       groupId: 3,
-      buttons: ['4', '5', '6', '-'],
+      buttons: ['4', '5', '6', String.fromCharCode(0x2212)],
     },
     {
       groupId: 4,
-      buttons: ['1', '2', '3', '+'],
+      buttons: ['1', '2', '3', String.fromCharCode(0x002b)],
     },
     {
       groupId: 5,
-      buttons: ['0', '.', '='],
+      buttons: ['0', String.fromCharCode(0x002e), String.fromCharCode(0x003d)],
     },
   ];
   const panelContents = buttonGroups.map(group => {
