ac4441775f7b2c7076b859d75104d3dd18731469
keshav-c <keshav1985@gmail.com>
2020-11-12 13:20:29 +0530

    Create the App component and render it

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import './style.css';
+import App from './components/App';
 
-function Calculator(props) {
-  const { name } = props;
-  return (
-    <h1>
-      This is
-      {' '}
-      { name }
-      {' '}
-      Calculator!
-    </h1>
-  );
-}
-
-Calculator.defaultProps = {
-  name: 'React',
-};
-
-Calculator.propTypes = {
-  name: PropTypes.string,
-};
-
-ReactDOM.render(<Calculator name="Second Milestone" />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
